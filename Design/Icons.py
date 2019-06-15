fdeebd9cbac1ec4cab68e3a94b63bcc89766dd52
Ajan Prabakar <ajanprabakar@gmail.com>
2019-06-15 04:04:39 -0400

    Made icons image files local in the Design folder

--- Design/Icons.py
+++ Design/Icons.py
@@ -1,4 +1,7 @@
-SettingsIcon = 'C:\GUIIcons\settings.png'
-PillIcon = 'C:\GUIIcons\pill.png'
-BackIcon = 'C:\GUIIcons\back.png'
-HomeIcon = 'C:\GUIIcons\home.png'
+SettingsIcon = 'Design\settings.png'
+PillIcon = 'Design\pill.png'
+BackIcon = 'Design\back.png'
+HomeIcon = 'Design\home.png'
+SolidPillTypeIcon = 'Design\PillType.png'
+LiquidTypeIcon = 'Design\LiquidType.png'
+
