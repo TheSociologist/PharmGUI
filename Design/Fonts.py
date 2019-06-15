669f468ff2ff8dd873fb2c309130b62b75de8343
Program <Program@Programs-MacBook-Pro-3899.local>
2019-06-15 00:22:08 -0400

    added gitignore to prevent design directory changes from affecting local performance

--- Design/Fonts.py
+++ Design/Fonts.py
@@ -1,3 +1,3 @@
 #Have to install fonts on local computer in order to work
 
-DefaultFont = 'Design\\fonts\\RobotoCondensed-Regular.ttf'
+DefaultFont = 'RobotoCondensed-Regular.ttf'
--- Design/Icons.py
+++ Design/Icons.py
@@ -1,7 +1,7 @@
-SettingsIcon = 'Design\images\settings.png'
-PillIcon = 'Design\images\pill.png'
-BackIcon = 'Design\images\back.png'
-HomeIcon = 'Design\images\home.png'
-SolidPillTypeIcon = 'Design\images\PillType.png'
-LiquidTypeIcon = 'Design\images\LiquidType.png'
+SettingsIcon = 'settings.png'
+PillIcon = 'Design/images/pill.png'
+BackIcon = 'Design/images/back.png'
+HomeIcon = 'Design/images/home.png'
+SolidPillTypeIcon = 'Design/images/PillType.png'
+LiquidTypeIcon = 'Design/images/LiquidType.png'
 
