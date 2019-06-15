c10ea889f43109a35e919b09ecd0c2c22035d6b8
Program <Program@Programs-MacBook-Pro-3899.local>
2019-06-15 00:32:54 -0400

    nother test

--- Design/Icons.py
+++
@@ -1,7 +0,0 @@
-SettingsIcon = 'Design/images/settings.png'
-PillIcon = 'Design/images/pill.png'
-BackIcon = 'Design/images/back.png'
-HomeIcon = 'Design/images/home.png'
-SolidPillTypeIcon = 'Design/images/PillType.png'
-LiquidTypeIcon = 'Design/images/LiquidType.png'
-
